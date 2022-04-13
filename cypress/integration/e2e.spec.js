0ceeaa02d433d44f64d26f915e94cd35ada6121d
Poli Rodrigues <polifreire@gmail.com>
2022-04-13 10:34:09 -0300

    update e2e

--- cypress/integration/e2e.spec.js
+++ cypress/integration/e2e.spec.js
@@ -1,21 +1,31 @@
-/// <reference types="cypress" />
+/// <reference types="cypress"/>
+const perfil = require('../fixtures/perfil.json')
 
-context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
-    /*  Como cliente 
-        Quero acessar a Loja EBAC 
-        Para fazer um pedido de 4 produtos 
-        Fazendo a escolha dos produtos
-        Adicionando ao carrinho
-        Preenchendo todas opções no checkout
-        E validando minha compra ao final */
+context('Funcionalidade E2E', () => {
 
     beforeEach(() => {
-        cy.visit('/')
+        cy.visit('minha-conta')
     });
 
     it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
-        //TODO 
-    });
+        var quantidade = 4
+        cy.fixture('perfil').then(dados => {
+            cy.get('#username').type(dados.usuario)
+            cy.get('#password').type(dados.senha, { log: false })
+            cy.get('.woocommerce-form > .button').click()
 
+            cy.get('.page-title').should('contain', 'Minha conta')
+            cy.get('#primary-menu > .menu-item-629 > a').click()
+            cy.get('[class="product-block grid"]').first().click()
+            cy.get('.button-variable-item-XL').click()
+            cy.get('.button-variable-item-Red').click()
+            cy.get('.input-text').clear().type(quantidade)
+            cy.get('.single_add_to_cart_button').click()
+            cy.get('.woocommerce-message > .button').click()
+            cy.get('.quantity > .input-text').clear().type(quantidade)
+            cy.get('.actions > .clearfix > .pull-right').click()
+            cy.get('.checkout-button').click()
+        })
+    });
 
 })
